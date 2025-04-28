f34a0e265c0db3d9d54f1d9a95e84ca0a454e189
Fuyuki-Kuro <fuyuki.g21@gmail.com>
2025-04-28 18:31:02 -0300

    teste

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ from routers.frontend_api.client import frontend_api_router
 app = FastAPI()
 app.include_router(frontend_api_router)
 app.include_router(backend_api_router)
+app.iclude_router(frontend_api_router)
 
 
